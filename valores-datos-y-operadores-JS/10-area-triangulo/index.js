d9b1d49a2a892dc774c46ca72bc6e6ec4fa5ed95
rosalybt <62813657+rosalybt@users.noreply.github.com>
2020-08-13 17:04:09 -0400

    ejercicio 10 terminado JS

---
+++ valores-datos-y-operadores-JS/10-area-triangulo/index.js
@@ -0,0 +1,5 @@
+let base = prompt("Ingrese la base del triangulo")
+let altura = prompt("Ingrese la altura del triangulo")
+let resultado  = (base*altura)/2
+
+alert(`el area del triangulo es ${resultado}`)
