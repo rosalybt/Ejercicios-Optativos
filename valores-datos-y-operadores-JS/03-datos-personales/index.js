abf2fe0482b4cbf23601890449adb704a0d604b9
rosalybt <62813657+rosalybt@users.noreply.github.com>
2020-08-13 16:04:28 -0400

    ejercicio 3 terminado JS

---
+++ valores-datos-y-operadores-JS/03-datos-personales/index.js
@@ -0,0 +1,8 @@
+let nombre = prompt("Ingrese su nombre")
+let apellidos = prompt("Ingrese sus apellidos")
+let edad = prompt("Ingrese su edad")
+let nacionalidad = prompt("Ingrese su nacionalidad")
+let documento = prompt("Ingrese su documento")
+
+
+alert(`Nuevo usuario agregado al sistema: ${nombre} ${apellidos}, edad: ${edad}, ${nacionalidad}, ${documento}`)
