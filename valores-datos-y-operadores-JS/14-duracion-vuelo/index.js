f5e3814e0c90318bc13219ce867f051b067d3f57
rosalybt <62813657+rosalybt@users.noreply.github.com>
2020-08-13 18:58:52 -0400

    ejercicio 14 terminado JS

---
+++ valores-datos-y-operadores-JS/14-duracion-vuelo/index.js
@@ -0,0 +1,17 @@
+const aPies = 5.3
+const bici = 10
+const auto = 30.3
+
+let escala1 = prompt("Ingrese la primera escala del vuelo")
+let escala2 = prompt("Ingrese la segunda escala del vuelo")
+let escala3 = prompt("Ingrese la tercera escala del vuelo")
+
+let duracion1 = prompt("Ingrese la duracion de la primera escala")
+let duracion2 = prompt("Ingrese la duracion de la segunda escala")
+let duracion3 = prompt("Ingrese la duracion de la tercera escala")
+
+
+let duracionTotal = Number(duracion1) + Number(duracion2) + Number(duracion3)
+
+
+alert(` Escala a : ${escala1}, duracion ${duracion1} \n Escala a : ${escala2}, duracion ${duracion2} \n Escala a : ${escala3}, duracion ${duracion3} \n Duracion total del vuelo: ${duracionTotal}` )
