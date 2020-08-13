277ba5880c7aca18f417ff329154a8eb2e44c8ba
rosalybt <62813657+rosalybt@users.noreply.github.com>
2020-08-13 16:30:11 -0400

    ejercicio 5 terminado JS

---
+++ valores-datos-y-operadores-JS/05-direccion-completa/index.js
@@ -0,0 +1,7 @@
+let calle = prompt("Ingrese el nombre de la calle")
+let numero = prompt("numero")
+let departamento = prompt("departamento")
+let codigoPostal = prompt("Codigo Postal")
+let pais = prompt("pais")
+
+alert(`La dirección que ha ingresado es: Calle ${calle}, ${numero}, ${departamento}, ${codigoPostal}, ${pais}`)
