f19a00a9deb25530ef8c83d776b2665eb0417a13
rosalybt <62813657+rosalybt@users.noreply.github.com>
2020-08-13 19:42:20 -0400

    ejercicio 15 terminado JS

---
+++ valores-datos-y-operadores-JS/15-incremento/index.js
@@ -0,0 +1,9 @@
+let numeroPartida = Number( prompt("Ingrese el numero de partida"))
+let incremento = Number(  prompt("numero de incremento"))
+ 
+
+alert(`Incremento 1: ${numeroPartida}`)
+alert("Incremento 2: " + Number( numeroPartida = numeroPartida +incremento))
+alert("Incremento 3: " + Number( numeroPartida = numeroPartida +incremento))
+alert("Incremento 4: " + Number(numeroPartida = numeroPartida +incremento))
+alert("Incremento 5: " + Number( numeroPartida = numeroPartida +incremento))
