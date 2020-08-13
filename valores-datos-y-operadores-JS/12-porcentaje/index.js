abbb278d503e2f5e0737f603472b6f76ba802d7b
rosalybt <62813657+rosalybt@users.noreply.github.com>
2020-08-13 18:21:57 -0400

    ejercicio 12 terminado JS

---
+++ valores-datos-y-operadores-JS/12-porcentaje/index.js
@@ -0,0 +1,5 @@
+let numero = prompt("Ingrese un numero")
+let porciento = prompt("porciento a calcular")
+let resultado = numero * (porciento/100)
+
+alert(`El ${porciento}% de ${numero} es ${resultado}` )
