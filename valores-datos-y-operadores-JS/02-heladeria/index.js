4d27f7efed96dd03a975de4c18d1b3fa3005d022
rosalybt <62813657+rosalybt@users.noreply.github.com>
2020-08-13 15:46:20 -0400

    ejercicio 2 terminado JS

---
+++ valores-datos-y-operadores-JS/02-heladeria/index.js
@@ -0,0 +1,5 @@
+let sabor1 = prompt("Dime el primer sabor")
+let sabor2 = prompt("Dime el segundo sabor")
+let sabor3 = prompt("Dime el tercer sabor")
+
+alert("Aquí tiene su helado de "+sabor1 +", "+sabor2 +" y "+sabor3)
