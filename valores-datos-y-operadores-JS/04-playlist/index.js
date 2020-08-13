35071966ca928f8cac609657e5822d327dc8b7c5
rosalybt <62813657+rosalybt@users.noreply.github.com>
2020-08-13 16:16:08 -0400

    ejercicio 4 terminado JS

---
+++ valores-datos-y-operadores-JS/04-playlist/index.js
@@ -0,0 +1,8 @@
+let playlist = prompt("Nombre del playlist:")
+let cancion1 = prompt("Agregue la cancion 1/3:")
+let cancion2 = prompt("Agregue la cancion 2/3:")
+let cancion3 = prompt("Agregue la cancion 3/3:")
+
+
+alert(`se creo la playlist ${playlist} con las canciones '${cancion1}', '${cancion2}','${cancion3}`)
+
