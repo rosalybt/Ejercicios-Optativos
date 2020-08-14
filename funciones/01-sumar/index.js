3e373cc7f764ad20c42bd5f8121e9fb5f854502c
rosalybt <62813657+rosalybt@users.noreply.github.com>
2020-08-14 06:32:01 -0400

    ejercicio 01 terminado funcionesJS

---
+++ funciones/01-sumar/index.js
@@ -0,0 +1,11 @@
+
+let sumar= (num1, num2)=>{
+
+return num1 + num2 
+}
+
+
+let primerNumero = Number(prompt("Digite el primer numero"))
+let segundoNumero = Number(prompt("Digite el segundo numero"))
+
+alert(`la suma de ${primerNumero} + ${segundoNumero} = ${sumar(primerNumero,segundoNumero)}`)
