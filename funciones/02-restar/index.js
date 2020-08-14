4703ffd1ace108436c31f888d12cdd91de1fd165
rosalybt <62813657+rosalybt@users.noreply.github.com>
2020-08-14 06:34:48 -0400

    ejercicio 02 funcionesJS terminado

---
+++ funciones/02-restar/index.js
@@ -0,0 +1,11 @@
+
+let restar= (num1, num2)=>{
+
+    return num1 - num2 
+    }
+    
+    
+    let primerNumero = Number(prompt("Digite el primer numero"))
+    let segundoNumero = Number(prompt("Digite el segundo numero"))
+    
+    alert(`la resta de ${primerNumero} - ${segundoNumero} = ${restar(primerNumero,segundoNumero)}`)
