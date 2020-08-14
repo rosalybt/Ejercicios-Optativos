dd0021425336a410ca8588c81a872a64dfd075fe
rosalybt <62813657+rosalybt@users.noreply.github.com>
2020-08-14 06:37:33 -0400

    ejercicio 03 funcionesJS terminado

---
+++ funciones/03-multiplicar/index.js
@@ -0,0 +1,10 @@
+let multiplicar= (num1, num2)=>{
+
+    return num1 * num2 
+    }
+    
+    
+    let primerNumero = Number(prompt("Digite el primer numero"))
+    let segundoNumero = Number(prompt("Digite el segundo numero"))
+    
+    alert(`la multiplicacion de ${primerNumero} * ${segundoNumero} = ${multiplicar(primerNumero,segundoNumero)}`)
